ab60239bf31cf85053b60de7437d9ac51d4c5d1e
lazovchik <lazovchik@gmail.com>
2019-10-23 16:10:33 +0200

    HomePage.js done

---
+++ src/HomePage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+import {
+	Container, Col, Form,
+	FormGroup, Label, Input,
+	Button, Row,
+} from 'reactstrap';
+
+import './LogSignIn.css';
+import LogInForm from './LogInForm';
+
+class LogSignIn extends Component {
+
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<Container className="main-container">
+			</Container>
+		);
+	}
+
+} 	
