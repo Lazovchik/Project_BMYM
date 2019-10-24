622ff4e2160e6b58a9c1092fe7a3dcc16fa34c8f
lazovchik <lazovchik@gmail.com>
2019-10-24 11:55:43 +0200

    Basis done

---
+++ src/AccountPage/AccountPage.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+import {
+	Container, Col, Form,
+	FormGroup, Label, Input,
+	Button, Row,
+} from 'reactstrap';
+
+import './AccountPage.css';
+
+class AccountPage extends Component {
+
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<Container className="rounded home-main-container w-75 pb-5" fluid>
+				<Row>
+					<Col>
+							
+					</Col>
+					<Col>
+						
+					</Col>
+				</Row>
+			</Container>
+		);
+	}
+};
+
+export default AccountPage;
