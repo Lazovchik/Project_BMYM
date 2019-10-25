0f3d0070eeb7855f6248e465e18ffc297850c6ec
lazovchik <lazovchik@gmail.com>
2019-10-25 17:12:06 +0200

    Front Done - useless components

--- src/Components/TransferPage/TransferPage.js
+++ src/Components/TransferPage/TransferPage.js
@@ -13,7 +13,7 @@ class TransferPage extends Component {
 
 	render(){
 		return(
-			<Container className="rounded transfer-main-container w-75 pb-5" fluid>
+			<Container className="rounded transfer-main-container w-50 pb-5" fluid>
 				<Row>
 					<Col>
 						<TransferRequest/>
